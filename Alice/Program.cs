f7e58d4a4dab760a88c643d98785e5f44ab4ca04
Adriana Disano <adrianadisano@gmail.com>
2018-08-16 12:29:57 -0500

    FEATURES: -Added functionality to allow users to search multiple times. CODE: -Added code comments. -Broke down search feature into a method.

--- Alice/Program.cs
+++ Alice/Program.cs
@@ -6,27 +6,76 @@ namespace Alice
     {
         static void Main(string[] args)
         {
+            //prompt the user for input
             string aliceString = "Alice was beginning to get very tired of sitting by her sister on the bank," +
                 " and of having nothing to do: once or twice she had peeped into the book her sister was reading," +
                 " but it had no pictures or conversations in it, ‘and what is the use of a book,’" +
                 " thought Alice ‘without pictures or conversation?’";
             Console.WriteLine(aliceString);
-            Console.WriteLine("Type a term to search for in the above passage:");
 
-            string searchTerm = Console.ReadLine();
+            //initialize and set a placeholder value to searchTerm
+            //this will store the user's search term.
+            string searchTerm = "";
 
-            bool termFound = aliceString.ToUpper().Contains(searchTerm.ToUpper());
+            //initialize and set a placeholder value to termFound
+            //we'll use this in the next line... 
+            //... to tell the program whether or not the search term was found.
+            bool termFound = false;
 
-            if (termFound == true)
+            //allow the user to search for a term
+            Console.WriteLine("Type a term to search for in the passage:");
+            searchTerm = Console.ReadLine();
+
+            //use the FindTerm method to tell the program whether or not the search term was found.
+            termFound = FindTerm(searchTerm, termFound, aliceString);
+
+            //execute until the user presses ENTER without entering a term to search...
+            //... or closes the program.
+            //we do this to continue executing the if/else-if statement...
+            //... so that the user can search for 2+ terms without having to reopen the program.
+            while (searchTerm != "")
             {
-                Console.WriteLine("Term \"" + searchTerm + "\" was found.");
-            }
-            else if (termFound == false)
-            {
-                Console.WriteLine("Term \"" + searchTerm + "\" was not found.");
+                //execute if the user input was found within aliceString
+                if (termFound == true)
+                {
+                    //tell the user that their search term was found in the passage
+                    Console.WriteLine("Term \"" + searchTerm + "\" was found.");
+
+                    //allow the user to search again
+                    Console.WriteLine("Type a term to search for in the passage:");
+                    searchTerm = Console.ReadLine();
+
+                    //search for the term and update the value of termFound.
+                    //this will adjust the outcome of the if/else-if "loop."
+                    termFound = FindTerm(searchTerm, termFound, aliceString);
+                }
+                //execute if the user input was not found within aliceString
+                else if (termFound == false)
+                {
+                    //tell the user that their search term was not found in the passage
+                    Console.WriteLine("Term \"" + searchTerm + "\" was not found.");
+
+                    //allow the user to search again
+                    Console.WriteLine("Type a term to search for in the passage:");
+                    searchTerm = Console.ReadLine();
+
+                    //search for the term and update the value of termFound.
+                    //this will adjust the outcome of the if/else-if "loop."
+                    termFound = FindTerm(searchTerm, termFound, aliceString);
+                }
             }
 
-            Console.ReadLine();
         }
+
+        private static bool FindTerm(string searchTerm, bool termFound, string aliceString)
+        {
+            //determine whether the search query is contained within aliceString.
+            //achieve case-insensitivity by turning user input to ALLCAPS and aliceString to ALLCAPS...
+            //... before doing the comparison.
+            termFound = aliceString.ToUpper().Contains(searchTerm.ToUpper());
+
+            return termFound;
+        }
+
     }
 }
